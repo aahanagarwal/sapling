7a6794c75fee5dfed05d3e3d8a203513e96dfc6f
Aahan1337 <aahanagarwal87@gmail.com>
2022-07-20 16:55:02 +0530

    Add Typography

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -10,9 +10,6 @@ const Layout = ({ children, title }) => {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
-        <meta charSet="utf-8" />
-        <meta http-equiv="x-ua-compatible" content="ie=edge" />
-        <meta name="theme-color" content="#0a6cff" />
         <title>{title}</title>
       </Head>
       <div id="popupContainer"></div>
